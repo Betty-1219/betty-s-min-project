603588d068ab3fa09ec26c32df2cf12c6151af49
Betelhem Nurlachw <bettynure1@gmail.com>
2026-05-01 23:35:35 +0300

    Clean up comments in script.js
    Removed comments for clarity in script.js.

--- gdg min project 1/script.js
+++ gdg min project 1/script.js
@@ -3,10 +3,8 @@
     let budget=Number(prompt("Enter your total budget:"));
     let price =Number(prompt("Enter the item price:"));
     let saving =Number(prompt("enter your saving (optional, enter 0 if none):"));
-    //total money available
     let totalmoney=budget+saving;
     let message;
-    //decision using if/else
         if (price<=totalmoney){
          message="you can afford this item!" ;
          alert(message);   
